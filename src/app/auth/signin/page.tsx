69b41cb3f5172b61b13d9fedb9529ff382e1a176
AchrafLarbi <larbiachraf03@gmail.com>
2026-02-10 19:44:22 +0100

    remove miscr

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -330,24 +330,6 @@ export default function SignIn() {
                 <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
               </svg>
             </button>
-
-            {/* Microsoft */}
-            <button
-              type="button"
-              className={`w-12 h-12 rounded-full flex items-center justify-center transition-all duration-200 ${
-                isDark 
-                  ? "bg-white/10 hover:bg-white/20 border border-white/10" 
-                  : "bg-white hover:bg-gray-50 border border-gray-200 shadow-sm"
-              }`}
-              title="Microsoft"
-            >
-              <svg className="w-5 h-5" viewBox="0 0 23 23">
-                <path fill="#f35325" d="M1 1h10v10H1z"/>
-                <path fill="#81bc06" d="M12 1h10v10H12z"/>
-                <path fill="#05a6f0" d="M1 12h10v10H1z"/>
-                <path fill="#ffba08" d="M12 12h10v10H12z"/>
-              </svg>
-            </button>
           </div>
 
           {/* Sign Up Link */}
